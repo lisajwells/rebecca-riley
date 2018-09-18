8752327701e40810c28b1e6358dc3ec54dfa5aef
Lisa Wells <lisawells999@gmail.com>
2018-09-18 15:22:39 -0400

    add homepage flex props; add mq styles and enqueue

--- functions.php
+++ functions.php
@@ -6,9 +6,11 @@
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
  */
 
-function generatepress_child_enqueue_scripts() {
+function rriley_enqueue_scripts() {
 	if ( is_rtl() ) {
 		wp_enqueue_style( 'generatepress-rtl', trailingslashit( get_template_directory_uri() ) . 'rtl.css' );
 	}
+	/* media queries style */
+	wp_enqueue_style( 'rriley_media', trailingslashit(get_stylesheet_directory_uri() ) . '/style-mq.css' );
 }
-add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100 );
+add_action( 'wp_enqueue_scripts', 'rriley_enqueue_scripts', 100 );
